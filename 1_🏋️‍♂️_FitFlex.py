3f0be8de97fa8e3d107ef6d4a631803866dd9f02
Branden Adems <88533031+BrandenAdems@users.noreply.github.com>
2023-08-08 12:11:36 +0800

    Update 1_🏋️‍♂️_FitFlex.py

--- 1_🏋️‍♂️_FitFlex.py
+++ 1_🏋️‍♂️_FitFlex.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import FitFlex
 
-# Creating a page logo
-st.set_page_config(
-    page_title="FitFlex",
-    page_icon="🏋️‍♂️",
-    )
-
 def run_app():
 
     FitFlex.main()
